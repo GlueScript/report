b315c3b592559150ad68a074644cad18e27bf975
Tim Rodger <tim.rodger@gmail.com>
2014-12-07 11:11:30 +0000

    Fixes bug in ensuring input is an array

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -6,13 +6,21 @@ exports.report = function(input, callback) {
     var output = [];
 
     // ensure that input is an array, if not treat it as a single element array
-    if (!input instanceof Array) {
+    if (!(input instanceof Array)) {
         input = [input];
     }
 
     // use the first item in input as the table headers
     // test that it is an Object, then use keys()
     var first = input[0];
+    output.push(Object.keys(first));
 
+    for(var item in input) {
+        var temp = [];
+        for(var cell in input[item]){
+            temp.push(input[item][cell]);
+        }
+        output.push(temp);
+    }
     callback(output);
 };
--- test/report_test.js
+++ test/report_test.js
@@ -3,12 +3,27 @@ var filter = require('../src/lib/filter'),
 
 describe('Report', function() {
     describe('report', function() {
-        it('should respond with an array', function(){
+        it('should respond with an array', function() {
             var value = [
                 {a: '111', b: '222'}
             ];
             filter.report(value, function(result) {
+                assert.equal(value.length + 1, result.length);
+                assert.equal('a', result[0][0]);
+                assert.equal('b', result[0][1]);
+                assert.equal('111', result[1][0]);
+                assert.equal('222', result[1][1]);
+            });
+        });
+
+        it('should respond with an array for object input', function() {
+            var value = {name: 'Reggie', age: 44}; 
+            filter.report(value, function(result) {
                 assert.equal(2, result.length);
+                assert.equal('name', result[0][0]);
+                assert.equal('age', result[0][1]);
+                assert.equal('Reggie', result[1][0]);
+                assert.equal('44', result[1][1]);
             });
         });
     });
